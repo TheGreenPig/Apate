04fad3a8be792ce20a9ec42e4f8682b22cf1d0ab
TheGreenPig <tim.m.toller@gmail.com>
2021-09-01 00:45:00 +0200

    Fixed Multiline

--- Apate.plugin.js
+++ Apate.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name Apate
- * @version 1.0.8
+ * @version 1.0.9
  * @description Hide your secret Discord messages in other messages!
  * @author TheGreenPig & Aster
  * @source https://github.com/TheGreenPig/Apate/blob/main/Apate.plugin.js
@@ -33,17 +33,17 @@ module.exports = (() => {
 				discord_id: "427179231164760066",
 				github_username: "TheGreenPig"
 			}],
-			version: "1.0.8",
+			version: "1.0.9",
 			description: "Apate lets you hide messages in other messages! - Usage: coverText *hiddenText*",
 			github_raw: "https://raw.githubusercontent.com/TheGreenPig/Apate/main/Apate.plugin.js",
 			github: "https://github.com/TheGreenPig/Apate"
 		},
 		changelog: [
 			{
-				title: "Security Update",
+				title: "Multiline Fix",
 				type: "fixed",
 				items: [
-					"Fixed a serious XSS vulnerability",
+					"Fixed an issue where you couldn't write multiple lines in the cover- or hidden message.",
 				]
 			},
 		],
@@ -643,46 +643,45 @@ module.exports = (() => {
 									hiddenMessageDiv.textContent = data.hiddenMsg;
 
 
-
 									let imageRegex = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg|svg)/gi;
 									let urlRegex = /(https?:\/\/)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|(https?:\/\/)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 									let emojiRegex = /\[[ -~]+?:\d+\.(png|gif)\]/g; // [ -~] are all printable ascii characters
+
 									if (urlRegex.test(data.hiddenMsg)) {
 										let linkArray = data.hiddenMsg.match(urlRegex);
 										let hasImage = false;
 
-										hiddenMessageDiv.textContent = data.hiddenMsg;
 
 
 										for (let i = 0; i < linkArray.length; i++) {
 											if (imageRegex.test(linkArray[i]) && hasImage === false && this.settings.displayImage) {
 												//Message has image link
 												let imageLink = linkArray[i];
-												hiddenMessageDiv.innerHTML  = `${hiddenMessageDiv.innerHTML.replace(imageLink, "")}</br><img class="apateHiddenImg" src="${imageLink}"></img>`;
+												hiddenMessageDiv.innerHTML = `${hiddenMessageDiv.innerHTML.replace(imageLink, "")}</br><img class="apateHiddenImg" src="${imageLink}"></img>`;
 												hasImage = true;
 
 											}
 											else {
 												hiddenMessageDiv.innerHTML = hiddenMessageDiv.innerHTML.replace(linkArray[i],
 													`<a class="anchor-3Z-8Bb anchorUnderlineOnHover-2ESHQB" 
-														title="${linkArray[i]}" 
-														href="${linkArray[i]}" 
-														rel="noreferrer noopener" 
-														target="_blank" 
-														role="button" 
-														tabindex="0"">
-														${linkArray[i]}</a>`);
+													title="${linkArray[i]}" 
+													href="${linkArray[i]}" 
+													rel="noreferrer noopener" 
+													target="_blank" 
+													role="button" 
+													tabindex="0"">
+													${linkArray[i]}</a>`);
 											}
 										}
 									}
 
 									if (emojiRegex.test(data.hiddenMsg)) {
-										hiddenMessageDiv.textContent = data.hiddenMsg;
 
 										let emojiContainerClass = BdApi.findModule(m => Object.keys(m).length === 1 && m.emojiContainer).emojiContainer;
 										let emojiArray = data.hiddenMsg.match(emojiRegex);
 
 										for (let i = 0; i < emojiArray.length; ++i) {
+
 											let [emojiName, emojiId] = emojiArray[i].slice(1, emojiArray[i].length - 1).split(":");
 
 											if (!this.settings.animate) {
@@ -692,9 +691,13 @@ module.exports = (() => {
 											hiddenMessageDiv.innerHTML = hiddenMessageDiv.innerHTML.replace(emojiArray[i],
 												`<span class="${emojiContainerClass}" tabindex="0">
 													<img aria-label="${emojiName}" src="https://cdn.discordapp.com/emojis/${emojiId}?v=1" alt=":${emojiName}:" class="emoji">
-												</span>`);
+													</span>`);
 										}
 									}
+									
+									hiddenMessageDiv.innerHTML = hiddenMessageDiv.innerHTML.replace(/\\n/g, '<br>');
+
+								
 
 									hiddenMessageDiv.classList.remove("loading");
 									messageContainer.setAttribute("data-apate-hidden-message-revealed", "");
@@ -757,15 +760,20 @@ module.exports = (() => {
 					let input = await (async () => {
 						const textSegments = textArea?.querySelectorAll(`div > div > span[data-slate-object]`);
 						let input = "";
+						let newLine = false;
 
 						for (let textSegment of textSegments) {
 							switch (textSegment.getAttribute("data-slate-object")) {
 								case ("text"): {
-
-									input += textSegment.textContent + "\n";
+									if (newLine) {
+										input += "\n";
+									}
+									input += textSegment.textContent;
+									newLine = true;
 									break;
 								}
 								case ("inline"): {
+									newLine = false;
 									const emojiName = textSegment.querySelector("img.emoji")?.alt?.replace(/:/g, "");
 
 									const emojiText = this.discordEmojis?.[emojiName] || await (async () => {
@@ -800,7 +808,6 @@ module.exports = (() => {
 					let hiddenMessage = RegExpGroups?.hiddenMessage?.trim();
 					let invalidEndString = RegExpGroups?.invalidEndString?.trim();
 
-					console.log(hiddenMessage)
 					const editor = BdApi.getInternalInstance(textArea).return.stateNode.editorRef;
 
 					if (!coverMessage) {
@@ -845,6 +852,8 @@ module.exports = (() => {
 					else {
 						pswd = this.settings.password;
 					}
+					hiddenMessage = hiddenMessage.replace(/[^ \S]{2}|[^ \S]{1}/g, "\\n") //replace new line with actual \n
+					console.log(hiddenMessage)
 					hiddenMessage += "\u200b"; //used as a verification if the password was correct 
 
 					document.querySelector(".apateEncryptionKey")?.classList.add("calculating");
