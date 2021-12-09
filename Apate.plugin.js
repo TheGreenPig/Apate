637cce327c7dbfd13956c2acc6927c9fd5d77a10
TheGreenPig <67547385+TheGreenPig@users.noreply.github.com>
2021-12-09 11:30:40 +0100

    Update Zlib import

--- Apate.plugin.js
+++ Apate.plugin.js
@@ -75,21 +75,25 @@ module.exports = (() => {
 
 
 	return !global.ZeresPluginLibrary ? class {
+		constructor() { this._config = config; }
+		getName() { return config.info.name; }
+		getAuthor() { return config.info.authors.map(a => a.name).join(", "); }
+		getDescription() { return config.info.description; }
+		getVersion() { return config.info.version; }
 		load() {
-			BdApi.showConfirmationModal("Library plugin is needed",
-				`The library plugin needed for AQWERT'sPluginBuilder is missing. Please click Download Now to install it.`, {
-				confirmText: "Download",
+			BdApi.showConfirmationModal("Library Missing", `The library plugin needed for **${config.info.name}** is missing. Please click Download Now to install it.`, {
+				confirmText: "Download Now",
 				cancelText: "Cancel",
 				onConfirm: () => {
-					request.get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", (error, response, body) => {
-						if (error)
-							return electron.shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-
-						fs.writeFileSync(path.join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body);
+					require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+						if (error) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+						await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
 					});
 				}
 			});
-		};
+		}
+		start() { }
+		stop() { }
 	} : (([Plugin, Api]) => {
 		const plugin = (Plugin, Api) => {
 			//All modules needed. All are found at the start because finding modules can be resource intensive.
